2ada8a413bcfbeabb362f17f0b426abe3f21ec4b
Jasielprogramador <asierrosa@gmail.com>
2021-11-11 19:43:26 +0100

    viva peru

---
+++ src/main/java/App/controllers/viejo/AukerarikEz.java
@@ -0,0 +1,12 @@
+package App.controllers.viejo;
+
+public class AukerarikEz extends Exception {
+	//atributuak
+
+	//eraikitzailea(k)
+	public AukerarikEz() {
+		super();
+	}
+
+	//gainontzeko metodoak
+}
---
+++ src/main/java/App/controllers/viejo/ZutabeBeteta.java
@@ -0,0 +1,15 @@
+package App.controllers.viejo;
+
+public class ZutabeBeteta extends Exception {
+	//atributuak
+
+	//eraikitzailea(k)
+	public ZutabeBeteta() {
+		super();
+	}
+
+	//gainontzeko metodoak
+	public void inprimatu() {
+		System.out.println("Beteta ez dagoen zutabe bat aukeratu");
+	}
+}
