e86675c90c50fc2a5f738f9aeff533912a53bd3d
jorgementx <jordims2000@gmail.com>
2021-11-14 21:57:46 +0100

    cosas de sql

--- src/main/java/App/controller/db/DBKud.java
+++ src/main/java/App/controller/db/DBKud.java
@@ -8,25 +8,41 @@ import java.util.Properties;
 
 public class DBKud {
 
+    private static final DBKud nDBKud = new DBKud();
     Connection conn = null;
 
-    private void conOpen() {
+    public static DBKud getDBKud(){
+        return nDBKud;
+    }
 
+    private DBKud() {
         Properties properties = Propietateak.lortuEzarpenak();
+        this.conOpen(properties.getProperty("dbpath"));
+    }
 
+    private void conOpen(String dbpath) {
         try {
-            Class.forName("com.mysql.cj.jdbc.Driver").getConstructor().newInstance();
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", properties);
-            conn.setCatalog(properties.getProperty("dbname"));
+            String url = "jdbc:sqlite:"+ dbpath ;
+            conn = DriverManager.getConnection(url);
 
-        } catch (SQLException ex) {
-            // handle any errors
-            System.out.println("SQLException: " + ex.getMessage());
-            System.out.println("SQLState: " + ex.getSQLState());
-            System.out.println("VendorError: " + ex.getErrorCode());
-        } catch (ClassNotFoundException | InvocationTargetException | IllegalAccessException | InstantiationException | NoSuchMethodException e) {
-            e.printStackTrace();
+            System.out.println("Database connection established");
+        } catch (Exception e) {
+            System.err.println("Cannot connect to database server " + e);
         }
+    }
+
+    private void conClose() {
+
+        if (conn != null)
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        System.out.println("Database connection terminated");
+
     }
 
     private ResultSet query(Statement s, String query) {
@@ -34,8 +50,7 @@ public class DBKud {
         ResultSet rs = null;
 
         try {
-            s.executeQuery(query);
-            rs = s.getResultSet();
+            rs = s.executeQuery(query);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -43,33 +58,26 @@ public class DBKud {
         return rs;
     }
 
-    // singleton patroia
-    private static DBKud instantzia = new DBKud();
-
-    private DBKud() {
-        this.conOpen();
-    }
-
-    public static DBKud getInstantzia() {
-        return instantzia;
-    }
-
     public ResultSet execSQL(String query) {
         int count = 0;
         Statement s = null;
         ResultSet rs = null;
+
         try {
             s = (Statement) conn.createStatement();
             if (query.toLowerCase().indexOf("select") == 0) {
                 // select agindu bat
                 rs = this.query(s, query);
+
             } else {
                 // update, delete, create agindu bat
                 count = s.executeUpdate(query);
+                System.out.println(count + " rows affected");
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return rs;
     }
 }
--- src/main/java/App/controller/db/Konekta4FXDBKud.java
+++ src/main/java/App/controller/db/Konekta4FXDBKud.java
@@ -1,4 +1,37 @@
 package App.controller.db;
 
+import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Konekta4FXDBKud {
+
+   private static final Konekta4FXDBKud instantzia = new Konekta4FXDBKud();
+
+   public static  Konekta4FXDBKud getInstantzia(){
+       return instantzia;
+   }
+
+   public ArrayList<String> getPuntuazioak(){
+        String query = "select winner, points from puntuazioak order by points desc limit 10";
+        ResultSet rs = DBKud.getDBKud().execSQL(query);
+        ArrayList<String> urlList = new ArrayList<>();
+        if (rs != null) {
+            try {
+               while (rs.next()) {
+                   String emaitza = rs.getString("winner") + " : " + rs.getString("points");
+                   urlList.add(emaitza);
+               }
+           } catch(SQLException throwables){
+               throwables.printStackTrace();
+           }
+        }
+        return urlList;
+    }
+
+    public void insertPartida(String winner, String loser, int points){
+        String query = "insert into puntuazioak (winner, loser, points) values ('" +winner+ "','" +loser+ "'," +points+ ")";
+        DBKud.getDBKud().execSQL(query);
+    }
+
 }
