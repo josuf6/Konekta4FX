72f6eda5fb8392abb8c38389da867557e17c9485
olaza <olazarmike>
2021-11-15 13:34:13 +0100

    seleccion de colores

--- src/main/java/App/controller/ui/HasieraKud.java
+++ src/main/java/App/controller/ui/HasieraKud.java
@@ -1,10 +1,13 @@
 package App.controller.ui;
 
 import App.Konekta4FX;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
@@ -14,6 +17,12 @@ import java.util.ResourceBundle;
 public class HasieraKud implements Initializable {
 
     private Konekta4FX main;
+
+    @FXML
+    private ComboBox combo1;
+
+    @FXML
+    private ComboBox combo2;
 
     @FXML
     private TextField txt1;
@@ -39,5 +48,9 @@ public class HasieraKud implements Initializable {
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {}
+    public void initialize(URL location, ResourceBundle resources) {
+        ObservableList<String> koloreak = FXCollections.observableArrayList("Arrosa","Beltza","Zian","Gorria","Laranja","Morea","Urdina","Berdea","Horia");
+        combo1.setItems(koloreak);
+        combo2.setItems(koloreak);
+    }
 }
