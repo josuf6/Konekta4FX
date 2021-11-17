03860a403938729b0f8ad7e58f172e359a1d9fff
olaza <olazarmike>
2021-11-17 13:43:47 +0100

    no deja empezaar partida si no has puesto los nombres

--- src/main/java/App/controller/ui/HasieraKud.java
+++ src/main/java/App/controller/ui/HasieraKud.java
@@ -56,15 +56,21 @@ public class HasieraKud implements Initializable {
         combo2.setItems(koloreak);
         addTextLimiter(txt1, 10);
         addTextLimiter(txt2, 10);
+        btnBotoia.setDisable(true);
     }
 
-    private static void addTextLimiter(final TextField tf, final int maxLength) {
+    private void addTextLimiter(final TextField tf, final int maxLength) {
         tf.textProperty().addListener(new ChangeListener<String>() {
             @Override
             public void changed(final ObservableValue<? extends String> ov, final String oldValue, final String newValue) {
                 if (tf.getText().length() > maxLength) {
                     String s = tf.getText().substring(0, maxLength);
                     tf.setText(s);
+                }
+                if (txt1.getText().length() >= 1 && txt2.getText().length() >= 1 ){
+                    btnBotoia.setDisable(false);
+                }else{
+                    btnBotoia.setDisable(true);
                 }
             }
         });
